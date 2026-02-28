d0be1d21b6ae1098bb0e29983b24a76708195639
brandemic-design <web@brandemic.in>
2026-02-28 15:56:29 +0530

    Update homepage animations

--- homepage_animations.js
+++ homepage_animations.js
@@ -529,7 +529,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         onComplete: () => {
           if (textAnimated) return;
           textAnimated = true;
-          gsap.fromTo(".home_about-content", { scale: 0.8 }, { scale: 1, duration: 2, ease: "none" });
+          gsap.fromTo(".home_about-content", { scale: 0.8 }, { scale: 1, duration: 2, ease: "power2.out" });
           gsap.set(".home_about-content", { opacity: 1 });
           gsap.set(".home_about-text", { autoAlpha: 1 });
           if (split && split.words) {
